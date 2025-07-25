4ac7025d1448cbc4608179dea4f95d40e9710fa6
Paul Smith <mail@paulsmith.site>
2025-07-25 17:50:13 +0100

    Add some config for the frame launcher

--- app/config.js
+++ app/config.js
@@ -13,4 +13,20 @@ module.exports = {
   // Enable cookie-based session store (persists on restart)
   // Please note 4KB cookie limit per domain, cookies too large will silently be ignored
   useCookieSessionStore: 'false',
+
+  frame: {
+    userAgent: 'Mozilla/5.0 (Linux; Android 10; Pixel 2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Mobile Safari/537.36 nhsapp-android/1.26.2',
+    localURLs: ['localhost', '127.0.1', '::1', 'nhs-app-111-prototype'],
+    proxyHeaders: [
+      'x-content-type-options',
+      'x-powered-by',
+      'strict-transport-security',
+      'referrer-policy',
+      'access-control-allow-origin',
+      'access-control-allow-credentials',
+      'x-frame-options',
+      'content-security-policy'
+    ]
+  }
+
 };
